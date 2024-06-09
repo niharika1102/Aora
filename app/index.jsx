e0c7e2eb5e25980e2cf5c55758cee49221933158
niharika1102 <niharikag1102@gmail.com>
2024-06-09 17:47:47 +0530

    Nativewind classes running

--- app/index.jsx
+++ app/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {Text, View} from 'react-native';
 import { Link } from 'expo-router';
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Hello mate... Welcome to aora</Text>
+    <View style={tw `flex-1 items-center justify-center bg-white`}>
+      <Text style={tw `text-3xl`}>Aora</Text>
       <StatusBar style="auto"/>
       <Link href="/profile" style={{color: 'red'}}>Go to profile</Link>
     </View>
